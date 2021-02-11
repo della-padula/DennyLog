299c977048023bc1f7088a56595383001dcd2be4
denny.k <denny.k@kakaocorp.com>
2021-02-11 09:17:13 +0900

    [DLI-1] Framework Project 추가

---
+++ DennyLog/DennyLog/DennyLog.h
@@ -0,0 +1,18 @@
+//
+//  DennyLog.h
+//  DennyLog
+//
+//  Created by denny on 2021/02/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DennyLog.
+FOUNDATION_EXPORT double DennyLogVersionNumber;
+
+//! Project version string for DennyLog.
+FOUNDATION_EXPORT const unsigned char DennyLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DennyLog/PublicHeader.h>
+
+
